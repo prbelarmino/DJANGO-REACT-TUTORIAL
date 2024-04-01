63adf0ea8f4e514482def072eec33cc3e278b8a7
paulo-belarmino <Paulo.Roque@ifs.rwth-aachen.de>
2024-04-01 21:12:52 +0200

    downgrade

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,15 +2,7 @@ import Form from "../components/Form"
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import '../styles/Link.css'
 function Login() {
-    return (
-        <div className="container">
-            <Form route="/api/token/" method="login" />
-            <Link to="/Register" className="link">
-                Register
-            </Link>
-        </div>
-    )
-    
+    return <Form route="/api/token/" method="login" />
 }
 
 export default Login
