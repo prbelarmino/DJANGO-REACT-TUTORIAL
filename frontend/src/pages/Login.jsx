2086830fd65ce141487730b507468563f9489c5b
paulo-belarmino <Paulo.Roque@ifs.rwth-aachen.de>
2024-04-01 21:41:52 +0200

    add register link in the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,15 @@ import Form from "../components/Form"
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import '../styles/Link.css'
 function Login() {
-    return <Form route="/api/token/" method="login" />
+    return (
+        <div  className="container">
+            <Form route="/api/token/" method="login" />
+            <Link to="/Register" className="link">
+                Register
+            </Link>
+        </div>
+    )
+    
 }
 
 export default Login
